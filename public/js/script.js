703b63aa29a1ba426b783e0862abfb6951d0a6ed
Prometteur <prometteur@pop-os.localdomain>
2022-07-01 12:29:55 +0530

    commited

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+document.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    const search = document.getElementById("search").value;
+
+    fetch(`/weather?search=${search}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error)
+                console.log(data.error);
+            else {
+                console.log(data.location);
+                document.getElementById("co-rdi").innerText=`${data.location.lat} and ${data.location.long}`;
+                document.getElementById("location").innerText=data.address.location;
+                document.getElementById("condition").innerText=data.address.condition;
+            }
+        })
+    })
+
+    document.getElementById("search").value = "";
+});
---
+++ src/app.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const forcast = require("./utils/forcast");
+const geocode = require("./utils/giocode");
+const request = require("request");
+
+// // console.log(__dirname);
+// console.log(path.join(__dirname,"../public"));
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//define path for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handelbars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather App",
+        name: "andrew work"
+    })
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About App",
+        name: "stephen playming"
+    })
+});
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help App",
+        name: "max stemyer"
+    })
+});
+
+app.get("/weather", (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "you must provide address"
+        });
+    }
+
+    geocode(req.query.search, (error, location) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forcast(location, (error, address) => {
+            if (error) {
+                return res.send({ error });
+            }
+            res.send({
+                location,
+                address
+            });
+        })
+    });
+});
+
+app.get("/products", (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "you must provide search term"
+        });
+    }
+    console.log(req.query);
+    res.send({
+        products: []
+    });
+});
+
+app.get("/help/*", (req, res) => {
+    res.send("help article not found");
+})
+
+app.get("*", (req, res) => {
+    res.send("my 404 page");
+})
+
+app.listen(port, () => console.log("server is run on port:" + port));
